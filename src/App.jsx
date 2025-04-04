717887d1f8b9106626a4341e153ed3d6c20085a0
eyad <eyadswailam12345@gmail.com>
2025-04-04 02:15:48 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import './css/App.css'
 import Favorites from './pages/Favorites'
 import Home from './pages/Home'
-import {Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import { MovieProvider } from './contexts/MovieContext'
+
 function App() {
-
-
   return (
-    
     <MovieProvider>
-      <NavBar />
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/favorites' element={<Favorites/>} />
-      </Routes>
-    </main>
+      <Router basename="/MovieSite">  
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </Router>
     </MovieProvider>
   )
 }
+
 export default App
